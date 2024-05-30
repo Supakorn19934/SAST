eacf6b2de2c87ddf86b5c248d50016c2a681844f
Supakorn19934 <128338520+Supakorn19934@users.noreply.github.com>
2024-05-30 19:24:38 +0700

    Fix

--- vulnerable_code.py
+++ vulnerable_code.py
@@ -1,4 +1,21 @@
-from flask import Flask, request
+# from flask import Flask, request
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'Hello, Flask!'
+
+# @app.route('/echo', methods=['GET'])
+# def echo():
+#     user_input = request.args.get('input')
+#     return f'You said: {user_input}'
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+from flask import Flask, request, escape
 
 app = Flask(__name__)
 
@@ -9,7 +26,8 @@ def home():
 @app.route('/echo', methods=['GET'])
 def echo():
     user_input = request.args.get('input')
-    return f'You said: {user_input}'
+    sanitized_input = escape(user_input)  # Sanitize user input to prevent injection attacks
+    return f'You said: {sanitized_input}'
 
 if __name__ == '__main__':
     app.run(debug=True)
