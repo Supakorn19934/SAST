93f8eeb85dcbb461bc751425ed3fc3f0d4d5d7d0
Supakorn19934 <128338520+Supakorn19934@users.noreply.github.com>
2024-06-25 19:00:03 +0700

    Fix

--- SAST.py
+++ SAST.py
@@ -1,24 +1,5 @@
-## Insecure
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hello, Flask!'
-
-@app.route('/echo', methods=['GET'])
-def echo():
-    user_input = request.args.get('input')
-    return f'You said: {user_input}'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-## Fix Code
-# from flask import Flask, request, escape
+# ## Insecure
+# from flask import Flask, request
 
 # app = Flask(__name__)
 
@@ -29,9 +10,27 @@ if __name__ == '__main__':
 # @app.route('/echo', methods=['GET'])
 # def echo():
 #     user_input = request.args.get('input')
-#     sanitized_input = escape(user_input)  # Sanitize user input to prevent injection attacks
-#     return f'You said: {sanitized_input}'
+#     return f'You said: {user_input}'
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
-#
+
+
+
+# Fix Code
+from flask import Flask, request, escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, Flask!'
+
+@app.route('/echo', methods=['GET'])
+def echo():
+    user_input = request.args.get('input')
+    sanitized_input = escape(user_input)  # Sanitize user input to prevent injection attacks
+    return f'You said: {sanitized_input}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
