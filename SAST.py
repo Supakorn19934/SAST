99736b10e248b7f29c5940643d06ae3327e76b21
Supakorn19934 <128338520+Supakorn19934@users.noreply.github.com>
2024-08-05 17:14:46 +0700

    Update SAST.py

--- SAST.py
+++ SAST.py
@@ -1,23 +1,3 @@
-# ## Insecure
-# from flask import Flask, request
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return 'Hello, Flask!'
-
-# @app.route('/echo', methods=['GET'])
-# def echo():
-#     user_input = request.args.get('input')
-#     return f'You said: {user_input}'
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-
-# Fix Code
 from flask import Flask, request, escape
 
 app = Flask(__name__)
